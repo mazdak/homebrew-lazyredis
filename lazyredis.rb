266921d28c5cface4f9b323ecda6f34ac9c4f820
mazdak <mazdak@mac.com>
2025-05-21 14:26:55 -0400

    Initial checkin

---
+++ lazyredis.rb
@@ -0,0 +1,22 @@
+class YourSoftware < Formula
+  desc "A fast and lazy Terminal UI for inspecting and manipulating Redis databases"
+  homepage "https://github.com/mazdak/lazyredis"
+  url "https://github.com/mazdak/lazyredis/releases/download/v0.5.0/lazyredis-0.5.0-aarch64-apple-darwin.tar.gz"
+  sha256 "d22db8f3eba3fd51faf74f7d72fdd268ed23aa133a6d69a5986a4a061c0ca0e6"
+  license "MIT" # or other license
+
+  # Specify dependencies (if any)
+  # depends_on "dependency1"
+  depends_on "cargo" => :build # Build-time dependency
+
+  def install
+    # Installation instructions
+    bin.install "lazyredis" # Example: install a binary to Homebrew's bin directory
+    # Add other installation steps (e.g., copying files, running make)
+  end
+
+  test do
+    # Test to verify installation
+    system "#{bin}/lazyredis", "--version"
+  end
+end
